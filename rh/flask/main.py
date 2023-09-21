86e4bee3e6703b7d7817365bc08dea0ed834d284
Alexey Nechaev <nechaev.lexa@gmail.com>
2023-09-21 11:43:39 +0300

    add deploy

--- rh/flask/main.py
+++ rh/flask/main.py
@@ -2,13 +2,5 @@ from app import init_app
 from app.config import *
 
 if __name__ == "__main__":
-    print('test')
-    print(os.environ.get('TEST_SECRET'))
-    print('test 123')
-
-    for key, value in os.environ.items():
-        print(f'os:{key}: {value}')
-
-
     app = init_app()
     app.run('0.0.0.0', port="5000", debug=True)
