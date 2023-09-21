8d157f1f9c54f799bb362add067a454c84dc94f1
Alexey Nechaev <nechaev.lexa@gmail.com>
2023-09-22 00:52:22 +0300

    fix deploy

--- rh/flask/app/config.py
+++ rh/flask/app/config.py
@@ -2,13 +2,14 @@
 import json
 import os
 import sys
-
+import base64
 
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
-JSON_CONFIG_PATH = APP_PATH+"/config.json"
+JSON_CONFIG_PATH = APP_PATH+"/config.b64"
 try:
     with open(JSON_CONFIG_PATH, 'r') as file:
-        JSON_CONFIG = json.load(file)
+        contents = file.read()
+    JSON_CONFIG = json.loads(base64.b64decode(contents).decode('utf-8'))
 
 except FileNotFoundError:
     print(f'{JSON_CONFIG_PATH} not found')
