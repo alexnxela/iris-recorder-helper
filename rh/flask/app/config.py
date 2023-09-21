120a64748a46d0b90c41db01e908165ea8bf0fe6
Alexey Nechaev <nechaev.lexa@gmail.com>
2023-09-21 23:51:45 +0300

    fix deploy

--- rh/flask/app/config.py
+++ rh/flask/app/config.py
@@ -3,13 +3,6 @@ import json
 import os
 import sys
 
-print('test')
-print(os.environ.get('TEST_SECRET'))
-print('test 123')
-
-for key, value in os.environ.items():
-    print(f'os:{key}: {value}')
-
 
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
 JSON_CONFIG_PATH = APP_PATH+"/config.json"
